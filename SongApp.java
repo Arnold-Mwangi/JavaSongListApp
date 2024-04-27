73d5e66d2d2e7f3e724f9ca352b2b49381b748a1
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2024-04-27 12:17:43 +0300

    CHanged from py to java

---
+++ SongApp.java
@@ -0,0 +1,10 @@
+
+// Name: Arnold Mwangi
+// Date started: 27 April 2024
+
+
+class SongApp {
+    public static void main(String[] args) {
+        System.out.print("Song List 1.0 - by Arnold Mwangi \n");
+    }
+}
